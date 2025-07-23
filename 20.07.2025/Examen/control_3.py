a769a528971fd189c07dacce2d5b656f0f94fae8
Denis Tarasov <aleks16555@mail.ru>
2025-07-24 02:33:52 +0300

    control_3

--- 20.07.2025/Examen/control_3.py
+++ 20.07.2025/Examen/control_3.py
@@ -8,33 +8,53 @@
 # В остальных случаях _health просто принимает новое значение.
 # Добавьте новое свойство (только для чтения, без сеттера) is_alive, которое будет возвращать True, если здоровье героя больше 0, и False в противном случае.
 # Пример использования (продолжение):
-
-
 # hero2 = Hero("Гэндальф", 120)
-
 # print(f"Жив ли {hero2.name}? {hero2.is_alive}")
-
 # hero2.health = 50 # Используем сеттер
-
 # print(hero2)
-
-
 # hero2.take_damage(150) # Получает урон больше, чем есть здоровья
-
 # print(hero2)
-
 # print(f"Жив ли {hero2.name}? {hero2.is_alive}")
-
 
 # Ожидаемый результат:
-
 # Жив ли Гэндальф? True
-
 # Герой Гэндальф, Здоровье: 50
-
 # Гэндальф получил 150 урона. Осталось -100 здоровья.
-
 # Герой Гэндальф, Здоровье: 0
-
 # Жив ли Гэндальф? False
+
+class Hero:
+    def __init__(self, name, health):
+        self.name = name
+        self._health = health 
+
+    @property
+    def health(self):
+        return self._health
+    
+    @health.setter
+    def health(self, value):
+        if value <=0:
+            self._health = 0
+        else:
+            self._health = value
+
+    @property
+    def is_alive(self):
+        return self._health > 0
+    
+    def take_damage(self, amount):
+        self.health -= amount
+        print(f"{self.name} получил {amount} урона. Осталось {self.health} здоровья.")
+
+    def __str__(self):
+        return f"Герой {self.name}, Здоровье: {self.health}"
+    
+hero2 = Hero("Гэндальф", 120)
+print(f"Жив ли {hero2.name}? {hero2.is_alive}")  # Проверяем текущий статус
+hero2.take_damage(50)
+print(hero2)
+hero2.take_damage(150) # Получает урон больше,чем есть здоровья
+print(hero2)
+print(f"Жив ли {hero2.name}? {hero2.is_alive}")
 
