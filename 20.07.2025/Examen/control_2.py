fa20832cd22018fe7c0aa0afc48f2d921669b734
Denis Tarasov <aleks16555@mail.ru>
2025-07-23 11:49:23 +0300

    control_2

--- 20.07.2025/Examen/control_2.py
+++ 20.07.2025/Examen/control_2.py
@@ -1,38 +1,45 @@
 # Задание 2: Первое действие (10 минут)
 # Цель: Добавить метод экземпляра, который изменяет его состояние.
-
 # В классе Hero из предыдущего задания создайте метод take_damage(damage).
 # Этот метод должен принимать число damage (урон) и уменьшать на это значение атрибут health.
 # После получения урона, метод должен выводить в консоль сообщение: "<Имя> получил <X> урона. Осталось <Y> здоровья.".
 # Пример использования (продолжение):
-
-
 # # hero1 создан в предыдущем задании
-
 # hero1.take_damage(25)
-
 # print(hero1) # Проверяем, что здоровье изменилось
-
-
 # Ожидаемый результат:
-
 # Арагорн получил 25 урона. Осталось 75 здоровья.
-
 # Герой Арагорн, Здоровье: 75
 
 class Hero:
-    def __init__(self, damage, health):
-        self.__damage = damage
+    def __init__(self, name, health):
+        self.__name = name
         self.__health = health
 
-    def take_damage(self,damage):
-        self.health -= damage
-        print(f"{self.name}получил{damage}урона.Осталось{self.health}здоровья.")
+    def take_damage(self, damage):
+        self.__health -= damage
+        print(f"{self.__name} получил {damage} урона. Осталось {self.__health} здоровья.")
 
     def __str__(self):
-        return f'Герой{self.name},Здоровье: {self.health}'
-hero1 = Hero('Aragorn',100)
-hero1.take_damage(25)
-print(hero1)
+        return f'Герой {self.__name}, Здоровье: {self.__health}'
+
+# Создаем экземпляр героя
+hero1 = Hero('Aragorn', 100)
+hero1.take_damage(25)  # Проверяем получение урона
+print(hero1)  # Проверяем, что здоровье изменилось
+# class Hero:
+#     def __init__(self, name, health):
+#         self.__name = name
+#         self.__health = health
+
+#     def take_damage(self,damage):
+#         self.__health -= damage
+#         print(f"{self.__name}получил{damage}урона.Осталось{self.__health}здоровья.")
+
+#     def __str__(self):
+#         return f'Герой{self.__name},Здоровье: {self.__health}'
+# hero1 = Hero('Aragorn',100)
+# hero1.take_damage(25)
+# print(hero1)
 
 
